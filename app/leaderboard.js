25554316acc8de7d33269a88bd5ad5374f54057e
Ryan Barsatan <ryanbinfo@gmail.com>
2016-10-11 17:14:53 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,3 +1,29 @@
-/**
- * Created by Admin on 10/11/2016.
- */
+PlayersList  = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    },
+  });
+
+  Template.leaderboard.events({
+    'click li': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+
+  });
+
+}
+
+if(Meteor.isServer){
+
+}
